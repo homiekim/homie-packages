f4a9028248fd1a6be3e2e36087e5726a6cff2649
HomieKim <ghals0708@gmail.com>
2024-12-06 03:21:53 +0900

    modify name

---
+++ packages/homie-state/CHANGELOG.md
@@ -0,0 +1,25 @@
+# @siper/homie-state
+
+## 2.0.3
+
+### Patch Changes
+
+- test
+
+## 2.0.2
+
+### Patch Changes
+
+- test
+
+## 2.0.1
+
+### Patch Changes
+
+- patch
+
+## 2.0.0
+
+### Major Changes
+
+- hook
--- packages/homie-state/src/homie-state/hook.ts
+++ packages/homie-state/src/homie-state/hook.ts
@@ -6,79 +6,33 @@ import {
 } from "./state";
 import { State } from "./store";
 
-type Stores<T extends Record<string, any>> = {
-  [K in keyof T]: State<T[K]>;
-};
-
-function createStoreManager<T extends Record<string, any>>() {
-  let stores: Stores<T> | null = null;
-
-  return {
-    initialize(initialStates: T): void {
-      if (stores) {
-        throw new Error("Stores have already been initialized.");
-      }
-
-      stores = Object.keys(initialStates).reduce((acc, key) => {
-        const typedKey = key as keyof T;
-        acc[typedKey] = store(initialStates[typedKey]);
-        return acc;
-      }, {} as Stores<T>);
-    },
-    getStores(): Stores<T> {
-      if (!stores) {
-        throw new Error(
-          "Stores have not been initialized. Please call initialize first."
-        );
-      }
-      return stores;
-    },
-  };
-}
-
-const storeManager = createStoreManager<any>();
-
-export function createHomies<T extends Record<string, any>>(
-  initialStates: T
-): void {
-  storeManager.initialize(initialStates);
-}
-
-export function getHomie<T extends Record<string, any>, K extends keyof T>(
-  key: K
-): [T[K], (param: T[K] | ((prev: T[K]) => T[K])) => void] {
-  const stores = storeManager.getStores();
-  const targetStore = stores[key];
-  return useExternalState(targetStore);
-}
-
-export function getHomieValue<T extends Record<string, any>, K extends keyof T>(
-  key: K
-): T[K] {
-  const stores = storeManager.getStores();
-  const targetStore = stores[key];
-  return useExternalValue(targetStore);
-}
-
-export function getHomieSetter<
-  T extends Record<string, any>,
-  K extends keyof T,
->(key: K): (param: T[K] | ((prev: T[K]) => T[K])) => void {
-  const stores = storeManager.getStores();
-  const targetStore = stores[key];
-  return useSetExternalState(targetStore);
-}
-
-export function getAllHomies<T extends Record<string, any>>(): {
-  [K in keyof T]: T[K];
-} {
-  const stores = storeManager.getStores();
-  return Object.keys(stores).reduce(
-    (acc, key) => {
-      const typedKey = key as keyof T;
-      acc[typedKey] = stores[typedKey].get();
+export const createStores = <T extends Object>(initialStates: T) => {
+  const stores = Object.entries(initialStates).reduce(
+    (acc, [key, value]) => {
+      acc[key as keyof T] = store(value);
       return acc;
     },
-    {} as { [K in keyof T]: T[K] }
+    {} as Record<keyof T, State<T>>
   );
-}
+
+  const getHomie = (key: keyof T) => {
+    const _store = stores[key];
+    return useExternalState(_store);
+  };
+
+  const getHomieValue = (key: keyof T) => {
+    const _store = stores[key];
+    return useExternalValue(_store);
+  };
+
+  const getHomieSetter = (key: keyof T) => {
+    const _store = stores[key];
+    return useSetExternalState(_store);
+  };
+
+  const getHomies = () => {
+    return stores;
+  };
+
+  return { getHomie, getHomieValue, getHomieSetter, getHomies };
+};
